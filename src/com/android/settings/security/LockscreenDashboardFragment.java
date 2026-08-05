1deb46080c7c00f3c6c558e767100aa7af70ab99
Ido Ben-Hur <idoybh2@gmail.com>
2026-08-05 12:57:01 +0000

    Settings: Allow scheduling always on display [2/2]
    * Squash of
      yaap/packages_apps_YASP@30441ec
      yaap/packages_apps_YASP@b896aa6
      yaap/packages_apps_YASP@f500a60
    
    [jhonboy121]: adapted to A13 and use a preference controller to update the preference
    
    Change-Id: If736f5609ca660e3c9797a309c814d2dcd618d31
    Co-Authored-By: jhonboy121 <alfredmathew05@gmail.com>

--- src/com/android/settings/security/LockscreenDashboardFragment.java
+++ src/com/android/settings/security/LockscreenDashboardFragment.java
@@ -36,6 +36,7 @@ import androidx.annotation.VisibleForTesting;
 import com.android.settings.R;
 import com.android.settings.dashboard.DashboardFragment;
 import com.android.settings.display.AmbientDisplayNotificationsPreferenceController;
+import com.android.settings.display.AODSchedulePreferenceController;
 import com.android.settings.gestures.DoubleTapScreenPreferenceController;
 import com.android.settings.gestures.PickupGesturePreferenceController;
 import com.android.settings.notification.LockScreenNotificationPreferenceController;
@@ -111,6 +112,7 @@ public class LockscreenDashboardFragment extends DashboardFragment
         use(AmbientDisplayNotificationsPreferenceController.class).setConfig(getConfig(context));
         use(DoubleTapScreenPreferenceController.class).setConfig(getConfig(context));
         use(PickupGesturePreferenceController.class).setConfig(getConfig(context));
+        use(AODSchedulePreferenceController.class).setConfig(getConfig(context));
 
         mControlsContentObserver = new ContentObserver(
                 new Handler(Looper.getMainLooper())) {
