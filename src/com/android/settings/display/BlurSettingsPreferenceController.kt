DerpFest-AOSP/android_packages_apps_Settings

Settings: Add dedicated blur settings page
Replace the Display blur switch with a preview page based on Axion's
blur level settings, using DISABLE_WINDOW_BLURS and BLUR_INTENSITY.

Based on: https://github.com/AxionAOSP/android_packages_apps_Settings/commit/8b846147344ae72f36f7d5052de5b6a4d117f2ac

Co-authored-by: rmp22 <195054967+rmp22@users.noreply.github.com>
Signed-off-by: NurKeinNeid <nurkeinneid@derpfest.org>

src/com/android/settings/display/BlurSettingsPreferenceController.kt
@@ -1,0 +1,46 @@
+/*
+ * SPDX-FileCopyrightText: DerpFest AOSP
+ * SPDX-FileCopyrightText: AxionOS
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package com.android.settings.display
+
+import android.content.Context
+import android.provider.Settings
+import android.view.CrossWindowBlurListeners.CROSS_WINDOW_BLUR_SUPPORTED
+import com.android.settings.R
+import com.android.settings.core.BasePreferenceController
+
+class BlurSettingsPreferenceController(
+    context: Context,
+) : BasePreferenceController(context, KEY_BLUR_SETTINGS) {
+
+    override fun getAvailabilityStatus(): Int {
+        return if (CROSS_WINDOW_BLUR_SUPPORTED) AVAILABLE else UNSUPPORTED_ON_DEVICE
+    }
+
+    override fun getSummary(): CharSequence {
+        val blursEnabled = Settings.Global.getInt(
+            mContext.contentResolver,
+            Settings.Global.DISABLE_WINDOW_BLURS,
+            0,
+        ) == 0
+
+        if (blursEnabled) {
+            val intensity = Settings.System.getInt(
+                mContext.contentResolver,
+                Settings.System.BLUR_INTENSITY,
+                DEFAULT_BLUR_INTENSITY,
+            ).coerceIn(MIN_BLUR_INTENSITY, MAX_BLUR_INTENSITY)
+            return mContext.getString(R.string.blur_settings_summary_on, intensity)
+        }
+        return mContext.getString(R.string.blur_settings_summary_off)
+    }
+
+    companion object {
+        const val KEY_BLUR_SETTINGS = "blur_settings"
+        private const val MIN_BLUR_INTENSITY = 0
+        private const val MAX_BLUR_INTENSITY = 200
+        private const val DEFAULT_BLUR_INTENSITY = 100
+    }
+}

Project-PenguinOS/packages_apps_Settings

Change-Id: Ib4b73773fbcc0caabecbb1b4174ce5a749acc0ed
Co-Authored-By: rmp22 <195054967+rmp22@users.noreply.github.com>

src/com/android/settings/display/BlurSettingsPreferenceController.kt
@@ -1,0 +1,47 @@
+/*
+ * SPDX-FileCopyrightText: DerpFest AOSP
+ * SPDX-FileCopyrightText: AxionOS
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package com.android.settings.display
+
+import android.content.Context
+import android.provider.Settings
+import android.view.CrossWindowBlurListeners.CROSS_WINDOW_BLUR_SUPPORTED
+import com.android.settings.R
+import com.android.settings.core.BasePreferenceController
+
+class BlurSettingsPreferenceController(
+    context: Context,
+    key: String = KEY_BLUR_SETTINGS,
+) : BasePreferenceController(context, key) {
+
+    override fun getAvailabilityStatus(): Int {
+        return if (CROSS_WINDOW_BLUR_SUPPORTED) AVAILABLE else UNSUPPORTED_ON_DEVICE
+    }
+
+    override fun getSummary(): CharSequence {
+        val blursEnabled = Settings.Global.getInt(
+            mContext.contentResolver,
+            Settings.Global.DISABLE_WINDOW_BLURS,
+            0,
+        ) == 0
+
+        if (blursEnabled) {
+            val intensity = Settings.System.getInt(
+                mContext.contentResolver,
+                Settings.System.BLUR_INTENSITY,
+                DEFAULT_BLUR_INTENSITY,
+            ).coerceIn(MIN_BLUR_INTENSITY, MAX_BLUR_INTENSITY)
+            return mContext.getString(R.string.blur_settings_summary_on, intensity)
+        }
+        return mContext.getString(R.string.blur_settings_summary_off)
+    }
+
+    companion object {
+        const val KEY_BLUR_SETTINGS = "blur_settings"
+        private const val MIN_BLUR_INTENSITY = 0
+        private const val MAX_BLUR_INTENSITY = 200
+        private const val DEFAULT_BLUR_INTENSITY = 100
+    }
+}